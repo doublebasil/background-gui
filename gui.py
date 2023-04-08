64537e9e15784ed74169757c53c597c35c822857
Tom Flaherty <tom.flaherty03@gmail.com>
2023-04-08 13:21:18 +0100

    Image now automatically resizes, but code could be simplified. Window cannot be shrunk quickly as image gets in the way

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ from PyQt6.QtCore import QRunnable, Qt, QThreadPool
 import os
 import sys
 from time import time, sleep
+from PIL import Image
 
 # class BackgroundThread(QtCore.QThread):
 #     def __init__(self):
@@ -16,34 +17,52 @@ from time import time, sleep
 #             sleep(0.5)
 #             print("Background thread")
 
+# global windowWidth
+# global windowHeight
+# windowWidth = -1
+# windowHeight = -1
+
 class BackgroundThread2(QRunnable):
-    def __init__(self):
+    def __init__(self, mainObj):
         super().__init__()
+        self.mainObj = mainObj
         self.threadPool = QThreadPool()
+        self.isRunning = True
     def run(self):
-        for i in range(0, 10):
-            print("Thread")
-            sleep(1)
+        previousWidth = -1
+        previousHeight = -1
+        while self.isRunning:
+            newWidth = self.mainObj.size().width()
+            newHeight = self.mainObj.size().height()
+            if (newWidth != previousWidth) or (newHeight != previousHeight):
+                previousHeight = newHeight
+                previousWidth = newWidth
+                self.resizeImage(newWidth, newHeight)
+            sleep(0.2)
+    def resizeImage(self, windowWidth, windowHeight):
+        print("imageLabel" in self.mainObj.__dir__())
+        self.mainObj.setImage(self.mainObj.imageLabel, self.mainObj.imageInfo["filePath"])
 
 class GUI(QWidget):
     def __init__(self):
         super().__init__()
         # thread = backgroundThread = BackgroundThread()
-        self.threadpool = QThreadPool()
-        self.backgroundThread = BackgroundThread2()
-        self.threadpool.start(self.backgroundThread)
 
         self.setWindowTitle("GUI Thing")
 
-        initWidth = 400
-        initHeight = 400
-        self.previousWidth = initWidth
-        self.previousHeight = initHeight
+        initWindowWidth = 400
+        initWindowHeight = 300
 
         self.mainLayout = QHBoxLayout()
         self.setLayout(self.mainLayout)
 
         ## create image view window
+        self.imageInfo = {
+            "filePath": "",
+            "imageWidth": -1,
+            "imageHeight": -1,
+        }
+        self.imageProportionOfWindowWidth = 0.5 # Width of the winodw that the image will take up
         self.imageLabel = QLabel()
         self.mainLayout.addWidget(self.imageLabel)
         self.setImage(self.imageLabel, os.path.join(os.getcwd(),"test_image.jpg"))
@@ -57,26 +76,39 @@ class GUI(QWidget):
         ## --- TEMP ---
         testButton = QPushButton(text="button")
         self.sideMenuLayout.addWidget(testButton)
-        self.resize(initWidth, initHeight)
+        self.resize(initWindowHeight, initWindowHeight)
         ## ---  --  ---
+        
+        ## start background thread for updating image size when the window is resized
+        self.threadpool = QThreadPool()
+        self.backgroundThread = BackgroundThread2(self)
+        self.threadpool.start(self.backgroundThread)
+
+    def __del__(self):
+        ## stop the background thread
+        self.backgroundThread.isRunning = False
 
     def setImage(self, label, imagePath):
-        pixmap = QPixmap(imagePath)
-        pixmap = pixmap.scaled(400, 400, Qt.AspectRatioMode.KeepAspectRatio)
+        image = Image.open(imagePath)
+        self.imageInfo["imageWidth"] = image.width
+        self.imageInfo["imageHeight"] = image.height
+        self.imageInfo["filePath"] = imagePath
+        windowWidth = self.size().width()
+        desiredImageWidth = round(windowWidth * self.imageProportionOfWindowWidth)
+        pixmap = QPixmap(imagePath).scaled(desiredImageWidth, desiredImageWidth, Qt.AspectRatioMode.KeepAspectRatio)
         label.setPixmap(pixmap)
     
-    def resizeEvent(self, obj):
-        width = obj.size().width()
-        height = obj.size().height()
-        if self.previousWidth == width:
-            print("Equal")
-        else:
-            print("Not equal")
-        self.previousWidth = width
-        # print("w="+str(width)+", h="+str(height))
+    # def resizeEvent(self, obj):
+    #     width = obj.size().width()
+    #     height = obj.size().height()
 
     # def mouseReleaseEvent(self, obj):
     #     print("Window was clicked")
+
+    # def loadImage(self, imagePath):
+        
+
+
 
 
 
